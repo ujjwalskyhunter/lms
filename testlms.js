5c36f1fd70b1a9dbc03ea75cd4e9cc661538bbeb
Kala-kala <kalaunnatikala@gmail.com>
2020-10-02 11:35:29 +0530

    Update testlms.js

--- testlms.js
+++ testlms.js
@@ -1,4 +1,3 @@
 <div style="width: 600px; margin: auto; text-align: center; font-family: arial">
-  <p>And so, the unwritten law of the internet, that any experiment involving video/images must involve cats in one way or another, reared its head again. When would the internet's fascination with cats come to an end? Never. The answer is "Never".</p> 
-  <img src='https://media.giphy.com/media/Byana3FscAMGQ/giphy.gif' class='gif' />
+
 </div>
